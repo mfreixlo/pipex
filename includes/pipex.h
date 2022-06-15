395ddf6c1852dbca36fba02a2ebe3b86e532a55d
mfreixlo <martafreixolopes@gmail.com>
2022-06-15 15:21:41 +0100

    corrigir norminette

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,20 +6,19 @@
 /*   By: mfreixo- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 20:31:12 by mfreixo-          #+#    #+#             */
-/*   Updated: 2022/06/13 19:24:30 by mfreixo-         ###   ########.fr       */
+/*   Updated: 2022/06/15 14:48:47 by mfreixo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_H
 # define PIPEX_H
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <sys/wait.h>
-#include "./ft_printf/includes/ft_printf.h"
-
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <sys/wait.h>
+# include "./ft_printf/includes/ft_printf.h"
 
 typedef struct s_pipe
 {
@@ -30,14 +29,11 @@ typedef struct s_pipe
 	int		outfile;
 } t_pipe;
 
-void	clear_pipe(t_pipe *mypipe);
-int		paths(t_pipe *mypipe);
-int		executae(char **cmd, t_pipe mypipe);
-int		files(t_pipe *mypipe, char *infile, char *outfile);
-int		child1(t_pipe *mypipe, char *args);
-int		child2(t_pipe *mypipe, char *args);
-
-
-
+int	clear_pipe(t_pipe *mypipe);
+int	paths(t_pipe *mypipe);
+int	executae(char **cmd, t_pipe mypipe);
+int	files(t_pipe *mypipe, char *infile, char *outfile);
+int	child1(t_pipe *mypipe, char *args);
+int	child2(t_pipe *mypipe, char *args);
 
 #endif
--- sources/main.c
+++ sources/main.c
@@ -1,48 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfreixo- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/14 13:48:04 by mfreixo-          #+#    #+#             */
+/*   Updated: 2022/06/15 15:20:38 by mfreixo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/pipex.h"
 
-void clear_pipe(t_pipe *mypipe)
+int	clear_pipe(t_pipe *mypipe)
 {
-	int i;
+	int	i;
 
 	i = 0;
+	if (!mypipe->paths || !mypipe->paths[0])
+		return (0);
 	while (mypipe->paths[i])
 	{
 		free(mypipe->paths[i]);
 		i++;
 	}
 	free(mypipe->paths);
+	return (0);
 }
 
-int paths(t_pipe *mypipe)
+void	clear_split(char ***split)
 {
-	int i;
-	char **paths;
-//	char *aux;
-
+	int	i;
 
 	i = 0;
-	while((mypipe->env)[i])
+	while ((*split)[i])
+	{
+		free((*split)[i]);
+		i++;
+	}
+	free(*split);
+}
+
+int	paths(t_pipe *mypipe)
+{
+	int		i;
+	char	**paths;
+
+	i = 0;
+	while ((mypipe->env)[i])
 	{
 		if (!ft_strncmp((*mypipe).env[i], "PATH", 4))
 			paths = ft_split(&(mypipe->env[i])[5], ':');
 		i++;
 	}
-	mypipe->paths = malloc(sizeof(char*) * i);
+	mypipe->paths = malloc(sizeof(char *) * i);
+	if (!(mypipe->paths))
+		return (-1);
 	i = 0;
 	while (paths[i])
 	{
 		mypipe->paths[i] = ft_strjoin(paths[i], "/");
 		i++;
 	}
+	clear_split(&paths);
 	mypipe->paths[i] = NULL;
 	return (0);
 }
 
-int executa(char **cmd, t_pipe mypipe)
+int	executa(char **cmd, t_pipe mypipe)
 {
-	(void) cmd;
-	int i;
-	char *path;
+	int		i;
+	char	*path;
 
 	i = 0;
 	while (mypipe.paths[i])
@@ -51,74 +79,113 @@ int executa(char **cmd, t_pipe mypipe)
 		if (!access(path, F_OK))
 		{
 			execve(path, cmd, mypipe.env);
-			free(path); //se for diferente de -1 nem entra aqui, logo tenho de ver aquela cena do access para libertar esta memory :)
+			free(path);
 			return (0);
 		}
 		free(path);
 		i++;
 	}
+	ft_putstr_fd("zsh: command not found: ", 2);
+	ft_putstr_fd(cmd[0], 2);
+	ft_putstr_fd("\n", 2);
 	return (0);
 }
 
-int files(t_pipe *mypipe, char *infile, char *outfile)
+void put_error(char *str, char *file)
 {
+	ft_putstr_fd(str, STDERR_FILENO);
+	ft_putstr_fd(file, STDERR_FILENO);
+	ft_putstr_fd("\n", STDERR_FILENO);
+}
+
+int	files(t_pipe *mypipe, char *infile, char *outfile)
+{
+	char	**str;
+
+	if (access(infile, F_OK) == -1)
+	{
+		str = ft_split(infile, ' ');
+		put_error("zsh: no such file or directory: ", infile);
+		clear_split(&str);
+		return (-1);
+	}
+	mypipe->outfile = open(outfile, O_CREAT | O_RDWR | O_TRUNC, 0000644);
+	if (access(outfile, W_OK) == -1)
+	{
+		put_error("zsh: permission denied: ", outfile);
+		return (-1);
+	}
+	if (access(infile, R_OK) == -1)
+	{
+		put_error("zsh: permission denied: ", infile);
+		return (-1);
+	}
 	mypipe->infile = open(infile, O_RDONLY);
-	mypipe->outfile = open(outfile, O_CREAT | O_RDWR | O_TRUNC, 0000644);	
 	if (mypipe->infile == -1 || mypipe->outfile == -1)
 		return (-1);
 	else
 		return (0);
 }
 
-int child1(t_pipe *mypipe, char *args)
+int	child1(t_pipe *mypipe, char *args)
 {
-	char **cmd1;
+	char	**cmd1;
 
 	cmd1 = ft_split(args, ' ');
 	close(mypipe->pipe[0]);
 	dup2(mypipe->infile, STDIN_FILENO);
-	dup2(mypipe->pipe[1], 1);
+	dup2(mypipe->pipe[1], STDOUT_FILENO);
 	close(mypipe->infile);
 	executa(cmd1, *mypipe);
-	exit(1);
+	exit(0);
 }
 
 int	child2(t_pipe *mypipe, char *args)
 {
-	char **cmd2;
+	char	**cmd2;
 
 	cmd2 = ft_split(args, ' ');
 	close(mypipe->pipe[1]);
 	dup2(mypipe->pipe[0], STDIN_FILENO);
-	dup2(mypipe->outfile, 1);
+	dup2(mypipe->outfile, STDOUT_FILENO);
+	close(mypipe->outfile);
+	close(mypipe->pipe[1]);
 	executa(cmd2, *mypipe);
-
-
-	return (0);
+	exit(0);
 }
 
-int main(int argc, char **argv, char **env) {
-	
-	(void) argc;
-	(void) argv;
-	t_pipe mypipe;
-	int	pid1;
-	int pid2;
+int	main(int argc, char **argv, char **env)
+{	
+	t_pipe	mypipe;
+	int		pid1;
+	int		pid2;
 
+	if (argc != 5)
+		return (0);
 	mypipe.env = env;
 	paths(&mypipe);
-	files(&mypipe, argv[1], argv[4]);
+	if (files(&mypipe, argv[1], argv[4]) == -1)
+		return (clear_pipe(&mypipe));
 	pipe(mypipe.pipe);
 	pid1 = fork();
 	if (pid1 < 0)
-		return (printf("fuck\n"));
-	else if (pid1 == 0) //estamos no processo filho
+	{
+		clear_pipe(&mypipe);
+		perror("Pipe error ");
+	}
+	else if (pid1 == 0)
 		child1(&mypipe, argv[2]);
 	pid2 = fork();
 	if (pid2 < 0)
-		printf("fuck thiiiissss\n");
+	{
+		clear_pipe(&mypipe);
+		perror("Pipe error ");
+	}
 	else if (pid2 == 0)
 		child2(&mypipe, argv[3]);
-//	clear_pipe(&mypipe);
+	close(mypipe.pipe[0]);
+	close(mypipe.pipe[1]);
+	waitpid(pid1, NULL, 0);
+	waitpid(pid2, NULL, 0);
+	return (0);
 }
-
